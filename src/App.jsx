f54913abeac4cde59c52027854270a788f0f4a9f
way2nizam <nizamy710@gmail.com>
2026-03-17 11:16:07 +0530

    feat: setup vite project and cleanup maintenance files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>🚀 BookMiSpace</h1>
+      <p>We're working on something awesome. Stay tuned!</p>
+    </>
+  );
+}
+
+export default App;
